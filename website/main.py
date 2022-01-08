2b3fba41dc7a575471e9767eaf27f922261bda12
MiguelLameiras <mlameirasmlameiras@gmail.com>
2022-01-08 02:22:39 +0000

    versão utilizavel

--- website/main.py
+++ website/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template,request
 import matplotlib.pyplot as plt
 import io
 import base64
@@ -32,12 +32,42 @@ def read(file_path):
 def home():
     return render_template("index.html")
 
+@app.route("/", methods = ['POST'])
+def dados_input():
+    dados = request.form['data']
+
+    dados = str(dados)
+    dados = dados.replace(";"," ").split()
+    dados_string = dados
+    dados = [float(i) for i in dados]
+    print(dados_string)
+    x,y = [],[]
+
+    num_cols = 4
+    length = int((len(dados))/num_cols-num_cols)
+    for i in range(0,length):
+        for j in range(0,4):
+            if (j == 0):
+                x.append(dados[j+num_cols*i])
+            if (j == 1):
+                y.append(dados[j+num_cols*i])
+
+    plt.plot(x,y)
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plt.clf()
+
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    
+    return render_template("plot_custom.html", content = dados_string, graph = plot_url,size = length, num_cols = 4)
+
 @app.route('/plot')
 def build_plot():
     img = io.BytesIO()
 
     leitura = read("data.csv")
-
+    print(leitura)
     x,y = [],[]
 
     for i in leitura:
@@ -51,10 +81,11 @@ def build_plot():
     plt.plot(x,y)
     plt.savefig(img, format='png')
     img.seek(0)
+    plt.clf()
 
     plot_url = base64.b64encode(img.getvalue()).decode()
     
-    return render_template("plot.html", content = leitura, graph = plot_url)
+    return render_template("plot_excel.html", content = leitura, graph = plot_url)
 
 if __name__ == "__main__":
     app.run(port=8100)
